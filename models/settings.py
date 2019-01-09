62a9d9ed2d554e0eae1c7703d3d509c06aceeea8
holdamax <holda.max@gmail.com>
2019-01-09 14:36:27 +0200

    add some changes

--- models/settings.py
+++ models/settings.py
@@ -16,26 +16,3 @@ FILE_HANDLER.setLevel(logging.DEBUG)
 FILE_HANDLER.setFormatter(FORMATTER)
 
 LOGGER.addHandler(FILE_HANDLER)
-
-
-class LogginSettings:
-    def __init__(self):
-        self.LOGGER = logging.getLogger(__name__)
-        self.LOGGER.setLevel(logging.DEBUG)
-
-        self.FORMATTER = logging.Formatter('%(asctime)s -- %(module)s -- %(levelname)s -- %(message)s',
-                                      datefmt='%d/%m/%Y %H:%M:%S')
-        if not os.path.exists("logs/"):
-            os.makedirs("logs/")
-        self.FILE_HANDLER = logging.FileHandler(f'logs/{self.LOGGER.name}.log')
-        self.FILE_HANDLER.setLevel(logging.DEBUG)
-        self.FILE_HANDLER.setFormatter(self.FORMATTER)
-        self.LOGGER.addHandler(self.FILE_HANDLER)
-
-    def loggin(self, response):
-        code = response.status_code
-        if code == HTTPStatus.OK:
-            self.LOGGER.info(f'CREATING...Status code: {code}. Successful operation.')
-            return response
-        self.LOGGER.warning(f'CREATING...Status code: {code}. Something went wrong')
-        return response
