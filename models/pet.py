5bd4932735640681cdad8597fa99f1799bbea6f7
holdamax <holda.max@gmail.com>
2019-01-09 12:24:10 +0200

    - delete logging file - delete models file - settings file configured

--- models/pet.py
+++ models/pet.py
@@ -2,23 +2,8 @@
 
 from http import HTTPStatus
 import json
-import logging
-import os.path
 import requests
-
-
-LOGGER = logging.getLogger('pet_logs')
-LOGGER.setLevel(logging.DEBUG)
-
-FORMATTER = logging.Formatter('%(levelname)-8s [%(asctime)s] %(filename)-8s %(funcName)-10s '
-                              '[LINE:%(lineno)s] %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-if not os.path.exists("../logs/"):
-    os.makedirs("../logs/")
-FILE_HANDLER = logging.FileHandler(f'../logs/{LOGGER.name}.log')  # Save log to file
-FILE_HANDLER.setLevel(logging.DEBUG)
-FILE_HANDLER.setFormatter(FORMATTER)
-
-LOGGER.addHandler(FILE_HANDLER)
+from models.settings import LOGGER
 
 
 def convert_data_to_json(data: dict):
