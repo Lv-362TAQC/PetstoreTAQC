145c732d7292c6ed637942ed6b0c0309a5bd49ab
marianmagur <33460464+marianmagur@users.noreply.github.com>
2019-01-10 21:06:25 +0200

    Update and rename store_get_delete.py to store_get_del.py

--- models/store_get_del.py
+++ models/store_get_del.py
@@ -5,35 +5,30 @@
  Negative or non-integer values will generate API errors """
 
 import logging
-from http import HTTPStatus
 import requests
-
-BASE_URL = "https://petstore.swagger.io/v2/store"
+from settings import BASE_URL_ORD
 
 
 LOGGER = logging.getLogger()
 
 logging.basicConfig(filename="api.log",
                     level=logging.INFO,
-                    format="%(levelname)s - %(message)s",
+                    format="%(levelname)s %(asctime)s - %(message)s",
                     filemode='w')
 
 
 class Store:
     """Creating class for testing GET & DELETE methods."""
     def __init__(self):
-        self.url = BASE_URL
+        self.url = BASE_URL_ORD
 
     def storeget(self, order_id):
         """Sending request with order id"""
-        response = requests.get(BASE_URL + '/order/' + str(order_id))
-        if response.status_code == HTTPStatus.OK:
-            LOGGER.info("{} - {}".format('OK', response.json()))
-        else:
-            LOGGER.info("{} {} is {}".format(response, order_id, 'wrong ID value. Sorry'))
+        response = requests.get(self.url + str(order_id))
+        LOGGER.info("{} {} {}".format(response, order_id, response.json()))
         return response
 
     def storedelete(self, del_id):
         """DELETE data"""
-        response = requests.delete(BASE_URL + '/order/' + str(del_id))
+        response = requests.delete(self.url + str(del_id))
         return response
